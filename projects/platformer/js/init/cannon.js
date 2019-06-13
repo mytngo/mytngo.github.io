de64dc5ad0919f9fb4782539b022d5e60fdb5571
My Ngo <mtn1891@ppsbstudent.org>
2019-06-13 13:40:46 +0000

    create awesome platformer game

---
+++ projects/platformer/js/init/cannon.js
@@ -0,0 +1,41 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let cannon = window.opspark.cannon;
+    
+    /**
+     * init: Initialize all cannons.
+     * 
+     * Add as many cannons as necessary (at least 3) to make your level challenging. 
+     *
+     * The following functions are available to you:
+     *  cannon.create.onTop(xLocation);
+     *  cannon.create.onBottom(xLocation);
+     *  cannon.create.onLeft(yLocation);
+     *  cannon.create.onRight(yLocation);
+     */ 
+    cannon.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        cannon.create.onTop(450);
+        cannon.create.onBottom(450);
+        cannon.create.onLeft(250);
+        cannon.create.onRight(350);
+        cannon.create.onTop(550);
+        cannon.create.onTop(350);
+        cannon.create.onBottom(350);
+        cannon.create.onBottom(550);
+        cannon.create.onLeft(350);
+        cannon.create.onLeft(450);
+        cannon.create.onRight(450);
+        cannon.create.onRight(250);
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
